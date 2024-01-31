a249398b756ae63dd84b3bfd00bd6e706b01c134
Lucas Pablo Antiñolo <LucasPablo.Antinolo@alumnos.uai.edu.ar>
2024-01-30 23:33:37 -0300

    Update con mejoras de logica y organizacion.Delete README.md

--- CALCULADORA FORMS/Form1.cs
+++ CALCULADORA FORMS/Form1.cs
@@ -17,238 +17,108 @@ namespace CALCULADORA_FORMS
             InitializeComponent();
         }
 
-       Funciones funcionesExt = new Funciones();
+       LogicaCalculadora Calculadora = new LogicaCalculadora();
 
-        bool valPrimerNum = false;
-        string primerNum = "";
-        string segundoNum = "";
-        int operacion = 0;
-        int resultado = 0;
-
+        //Botones especiales de operar
         private void btnSuma_Click(object sender, EventArgs e)
         {
-            valPrimerNum = true;
-            operacion = 1;
+            Calculadora.generarOperacion(1);
         }
 
         private void btnResta_Click(object sender, EventArgs e)
         {
-            valPrimerNum = true;
-            operacion = 2;
+            Calculadora.generarOperacion(2);
         }
 
         private void btnDiv_Click(object sender, EventArgs e)
         {
-            valPrimerNum = true;
-            operacion = 3;
+            Calculadora.generarOperacion(3);
         }
 
         private void btnPor_Click(object sender, EventArgs e)
         {
-            valPrimerNum = true;
-            operacion = 4;
+            Calculadora.generarOperacion(4);
         }
-
-
 
         private void btnIgual_Click(object sender, EventArgs e)
         {
-            if (segundoNum != null)
+            string res = Calculadora.Igual();
+            if (res != "0")
             {
-                resultado = funcionesExt.operar(operacion, primerNum, segundoNum);
-                visor.Text = resultado.ToString();
-                primerNum = resultado.ToString();
-                segundoNum = "";
-                operacion = 0;
+                visor.Text = res;
             }
-            else
-            {
-                MessageBox.Show("Ingresar operacion deseada y otro numero");
-            }
-            }
-
-       
-
+        }
 
 
         //10 BUTTONS DE NUMEROS
         private void button0_Click(object sender, EventArgs e)
         {
-            if(valPrimerNum == false)
-            {
-                primerNum = funcionesExt.sumarValor("0", primerNum);
-                visor.Text = primerNum.ToString();
-            }
-            else
-            {
-                segundoNum = funcionesExt.sumarValor("0", segundoNum);
-                visor.Text = segundoNum.ToString();
-            }
+            visor.Text = Calculadora.refljearOperacion("0");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (valPrimerNum == false)
-            {
-                primerNum = funcionesExt.sumarValor("1", primerNum);
-                visor.Text = primerNum.ToString();
-            }
-            else
-            {
-                segundoNum = funcionesExt.sumarValor("1", segundoNum);
-                visor.Text = segundoNum.ToString();
-            }
+            visor.Text = Calculadora.refljearOperacion("1");
         }
 
         private void button2_Click(object sender, EventArgs e)
-        {
-            if (valPrimerNum == false)
-            {
-                primerNum = funcionesExt.sumarValor("2", primerNum);
-                visor.Text = primerNum.ToString();
-            }
-            else
-            {
-               segundoNum = funcionesExt.sumarValor("2", segundoNum);
-               visor.Text = segundoNum.ToString();
-            }
+        {   
+            visor.Text = Calculadora.refljearOperacion("2");
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (valPrimerNum == false)
-            {
-                primerNum = funcionesExt.sumarValor("3", primerNum);
-                visor.Text = primerNum.ToString();
-            }
-            else
-            {
-                segundoNum = funcionesExt.sumarValor("3", segundoNum);
-                visor.Text = segundoNum.ToString();
-            }
+            visor.Text = Calculadora.refljearOperacion("3");
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            if (valPrimerNum == false)
-            {
-                primerNum = funcionesExt.sumarValor("4", primerNum);
-                visor.Text = primerNum.ToString();
-            }
-            else
-            {
-                segundoNum = funcionesExt.sumarValor("4", segundoNum);
-                visor.Text = segundoNum.ToString();
-            }
+            visor.Text = Calculadora.refljearOperacion("4");
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            if (valPrimerNum == false)
-            {
-                primerNum = funcionesExt.sumarValor("5", primerNum);
-                visor.Text = primerNum.ToString();
-            }
-            else
-            {
-                segundoNum = funcionesExt.sumarValor("5", segundoNum);
-                visor.Text = segundoNum.ToString();
-            }
+            visor.Text = Calculadora.refljearOperacion("5");
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            if (valPrimerNum == false)
-            {
-                primerNum = funcionesExt.sumarValor("6", primerNum);
-                visor.Text = primerNum.ToString();
-            }
-            else
-            {
-                segundoNum = funcionesExt.sumarValor("6", segundoNum);
-                visor.Text = segundoNum.ToString();
-            }
+            visor.Text = Calculadora.refljearOperacion("6");
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            if (valPrimerNum == false)
-            {
-                primerNum = funcionesExt.sumarValor("7", primerNum);
-                visor.Text = primerNum.ToString();
-            }
-            else
-            {
-                segundoNum = funcionesExt.sumarValor("7", segundoNum);
-                visor.Text = segundoNum.ToString();
-            }
+            visor.Text = Calculadora.refljearOperacion("7");
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            if (valPrimerNum == false)
-            {
-                primerNum = funcionesExt.sumarValor("8", primerNum);
-                visor.Text = primerNum.ToString();
-            }
-            else
-            {
-                segundoNum = funcionesExt.sumarValor("8", segundoNum);
-                visor.Text = segundoNum.ToString();
-            }
+            visor.Text = Calculadora.refljearOperacion("8");
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            if (valPrimerNum == false)
-            {
-                primerNum = funcionesExt.sumarValor("9", primerNum);
-                visor.Text = primerNum.ToString();
-            }
-            else
-            {
-                segundoNum = funcionesExt.sumarValor("9", segundoNum);
-                visor.Text= segundoNum.ToString();
-            }
+           visor.Text = Calculadora.refljearOperacion("9");
         }
-
-
+        
+        public void cambiarVisor (string contenido)
+        {
+            visor.Text = contenido;
+        }
 
 
         //AC
         private void btnDel_Click(object sender, EventArgs e)
         {
-            if (valPrimerNum == false && primerNum.Length>0)
-            {
-                primerNum = primerNum.Remove(primerNum.Length-1);
-                visor.Text = primerNum.ToString();
-            }
-            else if(segundoNum.Length>1 && segundoNum.Length>0)
-            {
-                segundoNum =  segundoNum.Remove(segundoNum.Length-1);
-                visor.Text = segundoNum.ToString();
-            }
-            else
-            {
-                MessageBox.Show("No puede borrar");
-            }
-            
+            visor.Text = Calculadora.eliminar();
         }
-
 
         //Restart
         private void btnAC_Click(object sender, EventArgs e)
         {
-                primerNum = "";
-                segundoNum = "";
-                visor.Text = "";
-                valPrimerNum = false;
-           
+            Calculadora.limpiar();
+            visor.Text = "";
         }
     }
-
-
-
-
     }
 
---
+++ CALCULADORA FORMS/LogicaCalculadora.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CALCULADORA_FORMS
+{
+    public class LogicaCalculadora
+    {
+
+        bool isPrimerNum = false;
+        string primerNum = "";
+        string segundoNum = "";
+        int operacion = 0;
+        int resultado = 0;
+
+        public void generarOperacion(int op)
+        {
+            isPrimerNum=true;
+            operacion = op;
+        }
+
+        //Funcion para hacer la cuenta
+        public int operar(int operacion, string primerNum, string segundoNum)
+        {
+          
+            switch (operacion) {
+                case 1:
+                    return Convert.ToInt32(primerNum) + Convert.ToInt32(segundoNum);
+                case 2:
+                    return Convert.ToInt32(primerNum) - Convert.ToInt32(segundoNum);
+                case 3:
+                    return Convert.ToInt32(primerNum) / Convert.ToInt32(segundoNum);
+                case 4:
+                    return Convert.ToInt32(primerNum) * Convert.ToInt32(segundoNum);
+                default : return 0;
+            }
+        }
+
+        public string refljearOperacion(string nro)
+        {
+            if (!isPrimerNum)
+            {
+                primerNum = sumarValor(nro, primerNum);
+                return  primerNum.ToString() ;
+            }
+            else
+            {
+                segundoNum = sumarValor(nro, segundoNum);
+                return segundoNum.ToString();
+            }
+        }
+        //Funcion para registar los numeros a operar
+        public string sumarValor(string nro, string Num)
+        {
+            return Num + nro;
+        }
+
+        internal string eliminar()
+        {
+            if (isPrimerNum == false && primerNum.Length > 0)
+            {
+                primerNum = primerNum.Remove(primerNum.Length - 1);
+                return primerNum.ToString();
+            }
+            else if (segundoNum.Length > 0)
+            {
+                segundoNum = segundoNum.Remove(segundoNum.Length - 1);
+                return segundoNum.ToString();
+            }
+                return "";
+        }
+
+        internal string Igual()
+        {
+            if (segundoNum == null)
+            {
+                return "0";
+            }
+            else
+            {
+                resultado = operar(operacion, primerNum, segundoNum);
+                primerNum = resultado.ToString();
+                segundoNum = "";
+                operacion = 0;
+                return resultado.ToString();
+            }
+        }
+
+        internal void limpiar()
+        {
+            primerNum = "";
+            segundoNum = "";
+            isPrimerNum = false;
+        }
+    }
+}
